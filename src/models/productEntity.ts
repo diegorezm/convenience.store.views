d96940ba9b8b1d1b70f5d8fd90e89ba7d97e26e1
diegorezm <dmourarezende@gmail.com>
2024-04-20 22:53:12 -0300

    feat: entity form schema

--- src/models/productEntity.ts
+++ src/models/productEntity.ts
@@ -1,3 +1,4 @@
+import { z } from 'zod'
 import Product from "./product";
 
 export default interface ProductEntity {
@@ -11,3 +12,12 @@ export default interface ProductEntity {
 export interface ProductEntityDTO {
   name: string
 }
+
+export const productEntityFormSchema = z.object({
+  name: z.string()
+    .min(5, {
+      message: "Not enough characters.(less than 5)"
+    }).max(30, {
+      message: "Too many characters.(more than 30)"
+    }),
+})
