e9adb6c73e966a1e05f80398660e6c464ba0fc81
diegorezm <dmourarezende@gmail.com>
2024-04-20 22:55:40 -0300

    feat: add btn + query params

--- src/app/dashboard/productsTable/index.tsx
+++ src/app/dashboard/productsTable/index.tsx
@@ -14,6 +14,9 @@ import { ProductActionsParam } from "@/queryParams/productsQueryParams"
 import DeleteProductDropdown from "./DeleteProductDropdown"
 import CreateTransaction from "./CreateTransaction"
 import { getAllProductEntities } from "@/actions/productEntityActions"
+import { Button } from "@/components/ui/button"
+import { Plus } from "lucide-react"
+import CreateProductEntity from "./CreateProductEntity"
 
 export interface ProductDropdownProps {
   clearParams: () => void
@@ -25,6 +28,7 @@ export interface ProductDropdownWithUpdatesProps extends ProductDropdownProps {
 }
 
 export default function ProductsTable() {
+  const [createModal, setcreateModal] = useState(false)
   const [productEntities, setProductEntities] = useState<ProductEntity[]>([])
   const router = useRouter()
   const pathname = usePathname()
@@ -50,17 +54,26 @@ export default function ProductsTable() {
     const deleteProductParam = params.get(ProductActionsParam.deleteProduct) ?? ""
     const transactionsParm = params.get(ProductActionsParam.productTransaction) ?? ""
     if (params.has(EntityActionsParam.showProductInfo)) return <EntityInfoDropdown clearParams={clearParams} id={showInfoParam} />
-    if (params.has(EntityActionsParam.deleteProductEntity)) return <DeleteEntityDropdown clearParams={clearParams} id={deleteEntityParam} setProducts={setProductEntities}/>
-    if (params.has(EntityActionsParam.editProductEntity)) return <EditEntityDropdown clearParams={clearParams} id={editEntityParam} setProducts={setProductEntities}/>
+    if (params.has(EntityActionsParam.deleteProductEntity)) return <DeleteEntityDropdown clearParams={clearParams} id={deleteEntityParam} setProducts={setProductEntities} />
+    if (params.has(EntityActionsParam.editProductEntity)) return <EditEntityDropdown clearParams={clearParams} id={editEntityParam} setProducts={setProductEntities} />
     if (params.has(ProductActionsParam.deleteProduct)) return <DeleteProductDropdown clearParams={clearParams} id={deleteProductParam} />
     if (params.has(ProductActionsParam.productTransaction)) return <CreateTransaction id={transactionsParm} clearParams={clearParams} />
     return null
   }
-
+  const openCloseCreateModal = () => {
+    setcreateModal(!createModal)
+  }
   return (
     <section className="flex flex-col p-4 gap-2">
       {LoadModalComponents() != null && LoadModalComponents()}
-      <ProductsInputFilter setProducts={setProductEntities} products={productEntities} />
+      {createModal && <CreateProductEntity setProducts={setProductEntities} close={openCloseCreateModal} />}
+      <div className="w-full flex flex-row justify-between">
+        <ProductsInputFilter setProducts={setProductEntities} products={productEntities} />
+        <Button className="flex justify-center gap-2" onClick={openCloseCreateModal}>
+          <span>Create new product</span>
+          <Plus className="w-5 h-5" />
+        </Button>
+      </div>
       <DataTable data={productEntities} columns={entityColumns} />
     </section>
   )
